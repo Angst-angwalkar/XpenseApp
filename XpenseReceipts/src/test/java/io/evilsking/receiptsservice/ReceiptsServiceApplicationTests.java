24a9298b706ed8550c4f240c007db9d413eda942
Aniket Angwalkar <angwalkar.316@gmail.com>
2024-02-06 00:46:11 +0000

    Boilerplate code with base structure for Category, Notification, Reports, Receipts services.

---
+++ XpenseReceipts/src/test/java/io/evilsking/receiptsservice/ReceiptsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.evilsking.receiptsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
