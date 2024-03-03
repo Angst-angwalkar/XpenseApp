f8423078e3dbaea21fd0943efedb3f1f854c9d40
Aniket Angwalkar <angwalkar.316@gmail.com>
2024-03-03 21:28:45 +0000

    Added Helper functions file for current date to Expense Service.

---
+++ ExpenseService/src/main/java/io/evilsking/ExpenseService/Helpers/Helpers.java
@@ -0,0 +1,14 @@
+package io.evilsking.ExpenseService.Helpers;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+public class Helpers {
+
+    public Date getCurrentDate(){
+        Date in = new Date();
+        LocalDateTime ldt = LocalDateTime.ofInstant(in.toInstant(), ZoneId.systemDefault());
+        return Date.from(ldt.atZone(ZoneId.systemDefault()).toInstant());
+    }
+}
