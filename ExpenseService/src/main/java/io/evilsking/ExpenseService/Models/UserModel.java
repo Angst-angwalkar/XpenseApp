0a02aab8a96ce897280f25759f5916badf658260
Aniket Angwalkar <angwalkar.316@gmail.com>
2024-03-08 15:19:59 +0000

    Added user response dto to expense service.

---
+++ ExpenseService/src/main/java/io/evilsking/ExpenseService/Models/UserModel.java
@@ -0,0 +1,89 @@
+package io.evilsking.ExpenseService.Models;
+
+import javax.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
+
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name="User")
+@Table(
+        name="User"
+)
+public class UserModel extends RepresentationModel<UserModel> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(
+            name = "userId", nullable = false
+    )
+    private Long userId;
+
+
+    @Column(
+            name = "userName", nullable = false, length = 25
+    )
+    private String userName;
+
+    @Column(
+            name = "firstName", nullable = false
+    )
+    private String firstName;
+
+
+    @Column(
+            name = "middleName", nullable = true
+    )
+    private String middleName;
+
+    @Column(
+            name = "lastName", nullable = false
+    )
+    private String lastName;
+
+    @Column(
+            name = "age", nullable = false
+    )
+    private String age;
+
+    @Column(
+            name = "mobileNo", nullable = false, length = 25
+    )
+    private String mobileNo;
+
+
+    @Column(
+            name = "isActive",
+            columnDefinition = "boolean default false"
+    )
+    private Boolean isActive;
+
+    @Column(
+            name = "email", nullable = false, length = 25
+    )
+    private String email;
+
+
+    @Column(
+            name = "address1", nullable = false, length = 100
+    )
+    private String address1;
+
+    @Column(
+            name = "address2", nullable = true, length = 100
+    )
+    private String address2;
+
+
+
+
+
+
+}
---
+++ ExpenseService/src/main/java/io/evilsking/ExpenseService/dto/UserResponse.java
@@ -0,0 +1,28 @@
+package io.evilsking.ExpenseService.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserResponse extends RepresentationModel<UserResponse> {
+    private String userName;
+    private String firstName;
+    private String middleName;
+    private Boolean isActive;
+    private String lastName;
+    private String age;
+    private String mobileNo;
+    private String email;
+    private String address1;
+    private String address2;
+
+}
